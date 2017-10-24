76ee706e1cade17fb8788d2af70786205711f704
jgw96 <jgw9617@gmail.com>
2017-10-24 14:23:34 -0500

    first commit

---
+++ src/components/my-name/my-name.tsx
@@ -0,0 +1,22 @@
+import { Component, Prop } from '@stencil/core';
+
+
+@Component({
+  tag: 'my-name',
+  styleUrl: 'my-name.scss',
+  shadow: true
+})
+export class MyName {
+
+  @Prop() first: string;
+
+  @Prop() last: string;
+
+  render() {
+    return (
+      <div>
+        Hello, my name is {this.first} {this.last}
+      </div>
+    );
+  }
+}
